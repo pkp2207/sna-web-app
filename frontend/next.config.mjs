e47993fb1aa8e7ddc7a606266ae3aca44c12efdc
Param Kamlesh Pathak <parampathak47@gmail.com>
2025-03-12 14:08:31 +0530

    Deploying try2

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    serverRuntimeConfig: {
-      port: process.env.PORT || 3000, // Use Render’s assigned port
-    },
   };
   
   export default nextConfig;
